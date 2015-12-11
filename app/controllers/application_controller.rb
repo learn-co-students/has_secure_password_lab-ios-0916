13c9f3ef5fc8126eb0c2e8d1e3584a69ae6fa405
Ashi Krishnan <me@ashi.works>
2015-12-11 15:54:16 -0500

    Solution.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :current_user
+
+  def current_user
+    @user = (User.find_by(id: session[:user_id]) || User.new)
+  end
+
+  def logged_in?
+    current_user.id != nil
+  end
+
+  def require_logged_in
+    return redirect_to(controller: 'sessions', action: 'new') unless logged_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  resources :users
+
+  root 'welcome#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
