068069488df610b4bf86af7b5ea08fed4493b0ef
Blake Johnson <blake41@gmail.com>
2016-02-02 17:51:34 -0500

    need to change specs to rspec syntax and fixing some specs that weren't correct

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,47 +2,47 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'has a password field' do
-    assert_respond_to User.new, :password
+    expect(User.new).to respond_to(:password)
   end
 
   it 'has a name field' do
-    assert_respond_to User.new, :name
+    expect(User.new).to respond_to(:name)
   end
 
   it 'has a password confirmation field' do
-    assert_respond_to User.new, :password_confirmation
+    expect(User.new).to respond_to(:password_confirmation)
   end
 
   it 'is valid if password and password_confirmation match' do
     user = User.new
     user.password = user.password_confirmation = 'foo'
-    assert user.valid?    
+    expect(user.valid?).to be true    
   end
 
   it 'is valid if password is set and password_confirmation is nil' do
      user = User.new
      user.password = 'foo'
-     assert user.valid?
+     expect(user.valid?).to be true 
   end
 
   it "is invalid if password and password_confirmation are both non-nil and don't match" do
     user = User.new
     user.password = 'foo'
     user.password_confirmation = 'fo0'
-    refute user.valid?
+    expect(user.valid?).to be false
   end
 
   describe 'authenticate' do
     it 'returns the user if credentials match' do
       user = User.new
       user.password = 'foo'
-      assert user.authenticate('foo') == user
+      expect(user.authenticate('foo')).to eq(user)
     end
 
     it "returns falsey if credentials don't match" do
       user = User.new
       user.password = 'foo'
-      refute user.authenticate('fo0')
+      expect(user.authenticate('fo0')).to be_falsey 
     end
   end
 end
